d26e94d154afc6e0b338c0961c7cadcbb260a13f
HECKY-ROG\ricsi <ricsi998@gmail.com>
2025-04-01 20:42:28 +0200

    Start

---
+++ ceges_Demo/src/main/java/com/example/ceges_Demo/CegesDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.ceges_Demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CegesDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CegesDemoApplication.class, args);
+	}
+
+}
